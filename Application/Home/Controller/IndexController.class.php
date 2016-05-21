751ae36462b3e3b09fd6ff482ed53f21c2ee7886
fuli <iplayphp@163.com>
2016-05-21 11:22:37 +0800

    前端页面主体设计

---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Home\Controller;
+
+use Home\Controller\BaseController;
+
+class IndexController extends BaseController
+{
+    public function index()
+    {
+        $this->display();
+    }
+}
