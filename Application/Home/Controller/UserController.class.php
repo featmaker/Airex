c4d9ced68bce3d62aaf156d7f1c6002e7a1a07f5
patrick95 <lawcy@qq.com>
2016-05-23 19:30:19 +0800

    url js changes

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -7,12 +7,27 @@ use Home\Controller\BaseController;
 */
 class UserController extends BaseController
 {
+	/**
+	 *
+	 * 验证码生成
+	 */
+	public function captcha(){
+		$Verify = new \Think\Verify();
+		$Verify->fontSize = 30;
+		$Verify->length   = 4;
+		$Verify->useNoise = false;
+		$Verify->entry();
+	}
+
 
 	public function login(){
 		$this->display();
 	}
 
+
+
 	public function register(){
+
 		$this->display();
 	}
 }
