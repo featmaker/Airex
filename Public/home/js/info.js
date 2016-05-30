d10a52bcc23dc29c161bd1b017304b3cee6dddd1
LDZ <ldzwow@foxmail.com>
2016-05-30 13:41:54 +0800

    jq

--- Public/home/js/info.js
+++ Public/home/js/info.js
@@ -1,25 +1,58 @@
-window.onload = function() {
 
-	document.getElementById("toggle").onclick = function turn() {
+/*************  javascript  ***********
+  
+		window.onload = function() {
 
-		var hide = document.getElementById("themeHide"),
-			show = document.getElementById("themeShow"),
-			toggle=document.getElementById("toggle");
+			document.getElementById("toggle").onclick = function() {
 
-		if (hide.style.display === "none") {
-			console.log("hide:none ==> block");
-			hide.style.display = "block";
-			show.style.display = "none";
-			toggle.getElementsByTagName("a")[0].textContent="点击显示主题";
+				var hide = document.getElementById("themeHide"),
+					show = document.getElementById("themeShow"),
+					toggle = document.getElementById("toggle");
 
-		} else {
-			console.log("hide:block ==> none");
-			hide.style.display = "none";
-			show.style.display = "block";
-			toggle.getElementsByTagName("a")[0].textContent="点击隐藏主题";
+				if (hide.style.display === "none") {
+					console.log("hide:none ==> block");
+					hide.style.display = "block";
+					show.style.display = "none";
+					toggle.getElementsByTagName("a")[0].textContent = "点击显示主题";
+				} else {
+					console.log("hide:block ==> none");
+					hide.style.display = "none";
+					show.style.display = "block";
+					toggle.getElementsByTagName("a")[0].textContent = "点击隐藏主题";
+				}
+			}
 		}
 
+*/
 
 
-	}
-}
+
+/************* jQuery *************/
+
+
+$(document).ready(function() {
+
+// 显示隐藏切换
+	$("#toggle").click(function() {
+		
+		var hide = $("#themeHide"),
+			show = $("#themeShow");
+
+		if (hide.is(":hidden")) {
+			hide.show();
+			show.hide();
+			$("#toggle>a").html("点击显示主题");
+		} else {
+			hide.hide();
+			show.show();
+			$("#toggle>a").html("点击隐藏主题");
+		}
+	});
+
+
+
+
+
+
+
+});
