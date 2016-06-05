c6662d11feebe5c1122a9298d6d6a4ca87cfc1ae
LDZ <ldzwow@foxmail.com>
2016-06-05 23:27:13 +0800

    new.html<新建主题>

---
+++ Public/home/js/new.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+	$('#submit').click(function() {
+		var theme = $('#theme'),
+			content = $('#content'),
+			tip;
+
+		function showTip(tip) {
+			if (tip.is(':hidden')) {
+				tip.show();
+				setTimeout(function() {
+					tip.hide();
+				}, 2000);
+			}
+		}
+
+		//标题空白
+		if ((theme.val() === "") || (theme.val().match(/^\s*$/))) {
+			theme.focus();
+			tip = $('#tips-theme');
+			//内容空白
+		} else if ((content.val() === "") || content.val().match(/^\s*$/)) {
+			content.focus();
+			tip = $('#tips-content');
+		}
+		showTip(tip);
+
+		return false;
+	});
+});
