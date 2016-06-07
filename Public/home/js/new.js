00f62a90c2c84ef8113f66c31a6928865a3dac4d
LDZ <ldzwow@foxmail.com>
2016-06-07 22:16:31 +0800

    node-select

--- Public/home/js/new.js
+++ Public/home/js/new.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	var t;
+
+	// 验证
 	$('#submit').click(function() {
 		var theme = $('#theme'),
 			content = $('#content'),
-			tip;
+			tip = null;
 
 		function showTip(tip) {
 			$('.tip').hide();
@@ -25,8 +27,26 @@ $(document).ready(function() {
 			content.focus();
 			tip = $('#tips-content');
 		}
-		showTip(tip);
+		if (tip) {
 
-		return false;
+			showTip(tip);
+			return false;
+		}
 	});
+
+	// 节点选择
+	$('#node-button').click(function(e) {
+		var node = e.target.textContent;
+
+		$("#node option").each(function() {
+			$(this).attr("selected", false);
+			if ($(this).text() === node) {
+				$(this).attr("selected", true);
+				return false;
+			}
+		});
+	});
+
+
+
 });
